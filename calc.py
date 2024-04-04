aada09df99ba2fe546531f82e447d6b79f8039cc
권준형 <165387277+mkjunh02@users.noreply.github.com>
2024-04-04 21:47:10 +0900

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,5 +1,19 @@
 def main():
+    print("Let's implement division. Type two numbers for x and y")
+    
+    x = int(input("x > "))
+    y = int(input("y > "))
+       
+    if y == 0:
+        divide(x,y)
+    else:
+        print("%d / %d = %0.3f" % (x, y, divide(x, y)))
 
-def add():
+def divide(x, y):
+    if y == 0:
+        print("Error: cannot divide by zero.")
+    else:
+        return x/y        
 
-
+if __name__ == "__main__":
+    main()
